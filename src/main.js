57223f41b6529681ed6b9cac45e423365de4b478
kyle <karonami@126.com>
2018-04-09 00:40:23 +0800

    安装模块

--- src/main.js
+++ src/main.js
@@ -1,15 +1,27 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+
+// import './assets/font/iconfont.css'
+
 import Vue from 'vue'
 import App from './App'
+import { sync } from 'vuex-router-sync'
 import router from './router'
+import store from './store'
+import Element from 'element-ui'
+import 'normalize.css'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
+Vue.use(Element)
+
+sync(store, router)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
