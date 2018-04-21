50509a36999a4f1791118cca76779b73004b25a6
陈振杰 <karon.chen@ximalaya.com>
2018-04-21 18:12:43 +0800

    修改数据库字段

---
+++ sql/cs_2018-04-21.sql
@@ -0,0 +1,156 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4541
+#
+# http://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Host: 127.0.0.1 (MySQL 5.7.21)
+# Database: cs
+# Generation Time: 2018-04-21 10:12:09 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table applications
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `applications`;
+
+CREATE TABLE `applications` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `projectId` int(11) DEFAULT NULL,
+  `testerId` int(11) DEFAULT NULL,
+  `auditTime` datetime DEFAULT NULL,
+  `isPass` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+# Dump of table defects
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `defects`;
+
+CREATE TABLE `defects` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `projectId` int(11) DEFAULT NULL,
+  `testerId` int(11) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `type` int(11) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `addTime` datetime DEFAULT NULL,
+  `isUse` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+# Dump of table devices
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `devices`;
+
+CREATE TABLE `devices` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  `os` varchar(50) DEFAULT NULL,
+  `osVersion` varchar(50) DEFAULT NULL,
+  `manufacturer` varchar(50) DEFAULT NULL,
+  `testerId` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+LOCK TABLES `devices` WRITE;
+/*!40000 ALTER TABLE `devices` DISABLE KEYS */;
+
+INSERT INTO `devices` (`id`, `name`, `os`, `osVersion`, `manufacturer`, `testerId`)
+VALUES
+	(19,'iphone x','ios','11.3','apple',9),
+	(21,'honor 9','android','7.0','honor',9);
+
+/*!40000 ALTER TABLE `devices` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table projects
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `projects`;
+
+CREATE TABLE `projects` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `customerId` int(11) DEFAULT NULL,
+  `releaseTime` datetime DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `reward` varchar(255) DEFAULT NULL,
+  `device` varchar(255) DEFAULT NULL,
+  `start` datetime DEFAULT NULL,
+  `end` datetime DEFAULT NULL,
+  `testerNumber` int(11) DEFAULT NULL,
+  `isExamine` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+# Dump of table rewards
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `rewards`;
+
+CREATE TABLE `rewards` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `projectId` int(11) NOT NULL,
+  `grade` int(11) DEFAULT NULL,
+  `reward` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+# Dump of table users
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `phone` varchar(11) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `type` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+
+INSERT INTO `users` (`id`, `name`, `password`, `phone`, `email`, `type`)
+VALUES
+	(2,'0','0',NULL,'0','admin'),
+	(9,'1','1',NULL,'1','tester'),
+	(10,'2','2',NULL,'2','customer');
+
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
